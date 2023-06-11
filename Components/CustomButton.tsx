f7a550d1b297e71d5643428bb789b270c021d5ec
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-06-11 21:01:25 +0530

    Add header and footer

--- Components/CustomButton.tsx
+++ Components/CustomButton.tsx
@@ -2,13 +2,13 @@
 import { CustomButtonProps } from "@/types";
 import Image from "next/image";
 
-const CustomButton = ({title, containerStyles, handleClick}: CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick, btnType}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
-      type={"button"}
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
-      onClick={() => {}}
+      onClick={handleClick}
     >
       <span className={`flex-1`}>
         {title}
--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -24,6 +24,7 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
         </div>
       </div>
     </div>
