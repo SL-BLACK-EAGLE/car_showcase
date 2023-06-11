c717cfc42b8598493cf23a3bef2dba1f0f6d229c
Chathuranga Lakmal <chathudreamshms@gmail.com>
2023-06-11 21:19:27 +0530

    add constants folder and copy index.ts file from adrian's github

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flex flex-col text-black-100 mt-5 border-t border-gray-100'>
+        <div className='flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10'>
+            <div className='flex flex-col justify-start items-start gap-6'>
+                <Image src="/logo.svg" alt='logo' width={118} height={18} className='object-contain'  />
+                <p className='text-base text-gray-700'>
+                    Carhub 20223 <br />
+                    All rights reserved &copy;
+                </p>
+            </div>
+            <div className='footer__links'>
+                
+            </div>
+        </div>
+    </footer>
   )
 }
 
